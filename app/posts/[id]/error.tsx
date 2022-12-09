28de575855c5c9fd3b76fcc656fb05cc210d3bfc
Raul <51750327+mrexito@users.noreply.github.com>
2022-12-09 10:21:58 +0100

    Step5

---
+++ app/posts/[id]/error.tsx
@@ -0,0 +1,8 @@
+"use client"
+export default function PostError() {
+  return (
+    <div>
+      PostError
+    </div>
+  )
+}
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -2,13 +2,17 @@ import Link from "next/link";
 
 export default async function PostsPage() {
 
-const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
 
   const posts = await response.json()
 
+  if (posts.length > 0) {
+  throw new Error("Vow we have some posts...")
+  }
+
   return (
     <div>
-      <h2>postspage</h2>
+      <h2>posts Page</h2>
       {posts.map(post => {
         return <div key={post.id}>
           <Link href={`/posts/${post.id}`}>{post.id} {post.title}</Link>
