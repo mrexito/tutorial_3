f9aa479c51296b5f81a779ce9b4d059cb2d3f754
Raul <51750327+mrexito@users.noreply.github.com>
2022-12-09 09:30:19 +0100

    Step2

---
+++ app/posts/[id]/page.tsx
@@ -0,0 +1,15 @@
+
+async function getPostById(id) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+  const post = await response.json()
+  return post
+}
+export default async function PostWithIdPage({ params, searchparams }, cache:"no-store") {
+  const post = await getPostById(params.id)
+
+  return (
+    <div>PostWithIdPage
+      <pre>{ JSON.stringify(post,null,2)}</pre>
+    </div>
+  )
+}
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 export default async function PostsPage() {
 
@@ -8,8 +9,11 @@ const response = await fetch("https://jsonplaceholder.typicode.com/posts")
   return (
     <div>
       <h2>postspage</h2>
-      <pre>{JSON.stringify(posts, null, 2)} 
-      </pre>
+      {posts.map(post => {
+        return <div key={post.id}>
+          <Link href={`/posts/${post.id}`}>{post.id} {post.title}</Link>
+        </div>;
+      })}
     </div>
   )
 }
